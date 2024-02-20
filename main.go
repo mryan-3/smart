d0ff809bd5252d07d023ffe4dece4cd1c9a4ded4
mryan-3 <malawaryan@gmail.com>
2024-02-20 15:40:46 +0300

    Added GET to fetch specific meter_id

--- main.go
+++ main.go
@@ -20,6 +20,27 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
         c.JSON(http.StatusOK, whole_data)
 	})
+
+    r.GET("/api/v1/smart-meter-data/:meter_id", func(c *gin.Context) {
+        smart_meter_id := c.Param("id")
+        var data Smart_Data
+
+        for _, smd := range whole_data {
+            if smd.Meter_id == smart_meter_id{
+                data = smd
+            }
+        }
+        if data.Meter_id == ""{
+            c.JSON(http.StatusBadRequest, gin.H{
+                "error": "Meter ID not found",
+            })
+        }
+
+        c.JSON(http.StatusOK, data)
+    })
+
+
+
     r.POST("/books", func(c *gin.Context) {
         var smart_data Smart_Data
 
