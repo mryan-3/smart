9f803eb64af81f22ecbb5d9a233254ac8ea6aab6
mryan-3 <malawaryan@gmail.com>
2024-02-27 00:42:15 +0300

    Added CORS

--- main.go
+++ main.go
@@ -5,56 +5,32 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	//"net/http"
-    "github.com/mryan-3/smart/routes"
-    "github.com/mryan-3/smart/db"
+	"github.com/gin-contrib/cors"
+	"github.com/mryan-3/smart/db"
+	"github.com/mryan-3/smart/routes"
+    "log"
+    "os"
 )
-
-
-
-// Function to validate API key
-//func validateAPIKey(c *gin.Context) (bool, string, error) {
-	//apiKey := c.GetHeader("Authorization")
-	//if apiKey == "" {
-		//return false, "", errors.New("Missing Key")
-	//}
-	//role, ok := db[apiKey]
-
-	//if !ok {
-		//return false, "", errors.New("Invalid Key")
-	//}
-	//return true, role, nil
-//}
-
-//func apiKeyAuthMiddleware() gin.HandlerFunc {
-	//return func(c *gin.Context) {
-		//isValid, role, err := validateAPIKey(c)
-		//if err != nil {
-			//c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-				//"error": err.Error(),
-			//})
-		//}
-
-		//if !isValid {
-			//c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				//"error": "Invalid API Key",
-			//})
-			//return
-		//}
-
-		//c.Set("UserRole", role)
-		//c.Next()
-	//}
-
-//}
 
 
 func main() {
 	fmt.Println("hello world jones")
-    db.CreateClient()
+	db.CreateClient()
 	r := gin.New()
-    //r.Use(apiKeyAuthMiddleware())
-    r.POST("/api/v1/smart-meter-data", routes.CreateSmartData)
-    r.GET("/api/v1/smart-meter-data", routes.GetData)
-    r.GET("/api/v1/smart-meter-data/:meter-id", routes.GetDataById)
+    r.Use(cors.Default())
+
+
+
+	r.POST("/api/v1/smart-meter-data", routes.CreateSmartData)
+	r.GET("/api/v1/smart-meter-data", routes.GetData)
+	r.GET("/api/v1/smart-meter-data/:meter-id", routes.GetDataById)
+
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+    if err := r.Run(":" + port); err != nil {
+        log.Panicf("error: %s", err)
+    }
 	r.Run()
 }
