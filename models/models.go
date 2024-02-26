cf0f94aee09e53c8e1ea754df31559d1e36bdd8c
mryan-3 <malawaryan@gmail.com>
2024-02-26 23:34:36 +0300

    removed timestamp string from the model

--- models/models.go
+++ models/models.go
@@ -1,7 +1,6 @@
 package models
 
 type Smart_Data struct {
-    Timestamp   string `binding:"required" json:"timestamp"`
 	Meter_id    string `binding:"required" json:"meter_id"`
     Consumption string `binding:"required" json:"consumption"`
 }
